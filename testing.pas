fac32e3bcee81a2329adadbb2b04f7d6317df46d
Akmal Narendra Sakti <akmalns@users.noreply.github.com>
2018-04-08 17:53:18 +0700

    File Testing

---
+++ testing.pas
@@ -0,0 +1,28 @@
+program testing;
+
+uses F1load,f8,tipe;
+
+var
+a:resepnya;
+b:InvMentah;
+c:InvOlahan;
+d:Mentah;
+e:tSimulasi;
+f:Olahan;
+energi:integer;
+uang:longint;
+pemasukan:longint;
+
+begin
+	uang:=100;
+	pemasukan:=0;
+	writeln('Testing cok');
+	load(d,f,b,c,a,e);
+	write(' '); readln(energi);
+	writeln(energi);
+	jualResep(a,b,c,uang,pemasukan,energi);
+
+	writeln(uang);
+	writeln(pemasukan);
+	writeln(energi);
+end.
