224c6a7c587fae40a6e09595f8fbec3b09cba088
edgyballer <38172799+edgyballer@users.noreply.github.com>
2018-04-08 15:23:41 +0700

    Add files via upload

---
+++ F1load.pas
@@ -0,0 +1,164 @@
+unit F1load;
+
+interface
+
+uses tipe;
+procedure load (var tabBahanMentah : Mentah; var tabBahanOlahan : Olahan; var tabInvenMentah : InvMentah; var tabInvenOlahan : InvOlahan; 
+				var tabResepnya : Resepnya; var tabSimulasi : tSimulasi);
+
+implementation
+
+procedure load (var tabBahanMentah : Mentah; var tabBahanOlahan : Olahan; var tabInvenMentah : InvMentah; var tabInvenOlahan : InvOlahan; 
+				var tabResepnya : Resepnya; var tabSimulasi : tSimulasi);
+
+var
+	ext		: Text;
+	j		: Integer;
+	skip	: String;
+
+begin
+skip:=' ';
+	assign(ext, 'BahanMentah.txt');
+	reset(ext);
+	tabBahanMentah.neff:=0;
+	if EOF(ext) then
+		begin
+			writeln('File kosong');
+		end
+	else
+		begin
+		while not(EOF(ext)) do
+			begin
+				inc(tabBahanMentah.neff);
+				readln(ext, tabBahanMentah.tabMentah[tabBahanMentah.neff].Nama);
+				readln(ext, tabBahanMentah.tabMentah[tabBahanMentah.neff].Harga);
+				readln(ext, tabBahanMentah.tabMentah[tabBahanMentah.neff].Kadaluarsa);
+				readln(ext, skip);
+			end;
+			writeln('File BahanMentah.txt terbaca');
+		end;
+	close(ext);
+	
+	assign(ext,'BahanOlahan.txt');
+	reset(ext);
+	tabBahanOlahan.neff:=0;
+	if EOF(ext) then
+		begin
+			writeln('File kosong');
+		end
+	else
+		begin
+		while not (EOF(ext)) do
+			begin
+				inc(tabBahanOlahan.neff);
+				readln(ext, tabBahanOlahan.tabOlahan[tabBahanOlahan.neff].Nama);
+				readln(ext, tabBahanOlahan.tabOlahan[tabBahanOlahan.neff].Harga);
+				readln(ext, tabBahanOlahan.tabOlahan[tabBahanOlahan.neff].NButuh);
+					for j:=1 to tabBahanOlahan.tabOlahan[tabBahanOlahan.neff].NButuh do
+						begin
+							readln(ext, tabBahanOlahan.tabOlahan[tabBahanOlahan.neff].bahan[j]);
+						end;
+				readln(ext, skip);
+			end;
+			writeln('File BahanOlahan.txt terbaca');
+		end;
+	close(ext);
+	
+	assign(ext,'InvMentah.txt');
+	reset(ext);
+	tabInvenMentah.neff:=0;
+	if EOF(ext) then
+		begin
+			writeln('File kosong');
+		end
+	else
+		begin
+		while not (EOF(ext)) do
+			begin
+				inc(tabInvenMentah.neff);
+				readln(ext, tabInvenMentah.TabInvMentah[tabInvenMentah.neff].Nama);
+				readln(ext, tabInvenMentah.TabInvMentah[tabInvenMentah.neff].Tgl.hari, tabInvenMentah.TabInvMentah[tabInvenMentah.neff].Tgl.bulan, tabInvenMentah.TabInvMentah[tabInvenMentah.neff].Tgl.tahun);
+				readln(ext, tabInvenMentah.TabInvMentah[tabInvenMentah.neff].Jumlah);
+				readln(ext, skip);
+			end;
+			writeln('File InventoryMentah.txt sudah terbaca');
+		end;
+	close(ext);
+	
+	assign(ext,'InvOlahan.txt');
+	reset(ext);
+	tabInvenOlahan.neff:= 0;
+	if EOF(ext) then
+		begin
+			writeln('File Kosong');
+		end
+	else
+		begin
+		while not(EOF(ext)) do
+			begin
+				inc(tabInvenOlahan.neff);
+				readln(ext, tabInvenOlahan.TabInvOlahan[tabInvenOlahan.neff].Nama);
+				readln(ext, tabInvenOlahan.TabInvOlahan[tabInvenOlahan.neff].TglBuat.hari, tabInvenOlahan.TabInvOlahan[tabInvenOlahan.neff].TglBuat.bulan, tabInvenOlahan.TabInvOlahan[tabInvenOlahan.neff].TglBuat.tahun);
+				readln(ext, tabInvenOlahan.TabInvOlahan[tabInvenOlahan.neff].Jumlah);
+				readln(ext, skip);
+			end;
+			writeln('File InventoryOlahan.txt sudah terbaca');
+		end;
+	close(ext);
+	
+	assign(ext,'Resep.txt');
+	reset(ext);
+	tabResepnya.neff:=0;
+	if EOF(ext) then
+		begin
+			writeln('File Kosong');
+		end
+	else
+		begin
+		while not(EOF(ext)) do
+			begin
+				inc(tabResepnya.neff);
+				readln(ext, tabResepnya.TabResep[tabResepnya.neff].Nama);
+				readln(ext, tabResepnya.TabResep[tabResepnya.neff].Harga);
+				readln(ext, tabResepnya.TabResep[tabResepnya.neff].NButuh);
+					for j:=1 to tabResepnya.TabResep[tabResepnya.neff].NButuh do
+						begin
+							readln(ext, tabResepnya.TabResep[tabResepnya.neff].Olah[j]);
+						end;
+					readln(ext, skip);
+			end;
+			writeln('File Resep.txt sudah terbaca');
+		end;
+	close(ext);
+
+	assign(ext, 'Simulasi.txt');
+	reset(ext);
+	tabSimulasi.neff:=0;
+	if EOF(ext) then
+		begin
+			writeln('File kosong');
+		end
+	else 
+		begin
+			while not(EOF(ext)) do
+				begin
+					inc(tabSimulasi.neff);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].Nomor);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].Tgl.hari, tabSimulasi.Tab[tabSimulasi.neff].Tgl.bulan, tabSimulasi.Tab[tabSimulasi.neff].Tgl.tahun);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].HariHidup);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].Energi);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].KapasitasInventory);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].TotalBeliMentah);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].TotalOlahBuat);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].TotalOlahJual);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].TotalResepJual);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].TotalPemasukan);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].TotalPengeluaran);
+					readln(ext, tabSimulasi.Tab[tabSimulasi.neff].TotalUang);
+					readln(ext, skip);
+				end;
+				writeln('File Simulasi.txt terbaca');
+		end;
+	close(ext);
+end;
+end.
---
+++ F2exit.pas
@@ -0,0 +1,104 @@
+unit F2exit;
+
+interface
+
+uses tipe;
+procedure exit (var tabBahanMentah : Mentah; var tabBahanOlahan : Olahan; var tabInvenMentah : InvMentah; var tabInvenOlahan : InvOlahan; 
+				var tabResepnya : Resepnya; var tabSimulasi : tSimulasi);
+
+implementation
+
+procedure exit (var tabBahanMentah : Mentah; var tabBahanOlahan : Olahan; var tabInvenMentah : InvMentah; var tabInvenOlahan : InvOlahan; 
+				var tabResepnya : Resepnya; var tabSimulasi : tSimulasi);
+
+var
+	ext		: Text;
+	i,j		: Integer;
+	skip	: String;
+
+begin
+skip:=' ';
+assign (ext,'BahanMentah.txt');
+rewrite(ext);
+	for i:=1 to tabBahanMentah.Neff do
+		begin
+			writeln(ext, tabBahanMentah.TabMentah[i].Nama);
+			writeln(ext, tabBahanMentah.TabMentah[i].Harga);
+			writeln(ext, tabBahanMentah.TabMentah[i].Kadaluarsa);
+			writeln(ext, skip);
+		end;
+		close(ext);
+
+assign(ext,'BahanOlahan.txt');
+rewrite(ext);
+	for i:=1 to tabBahanOlahan.Neff do
+		begin
+			writeln(ext, tabBahanOlahan.TabOlahan[i].Nama);
+			writeln(ext, tabBahanOlahan.TabOlahan[i].Harga);
+			writeln(ext, tabBahanOlahan.TabOlahan[i].NButuh);
+				for j:=1 to tabBahanOlahan.TabOlahan[i].NButuh do
+					begin
+						writeln(ext, tabBahanOlahan.TabOlahan[i].bahan[j]);
+					end;
+			writeln(ext, skip);
+		end;
+		close(ext);
+
+assign (ext,'InvMentah.txt');
+rewrite(ext);
+	for i:=1 to tabInvenMentah.Neff do
+		begin
+			writeln(ext, tabInvenMentah.TabInvMentah[i].Nama);
+			writeln(ext, tabInvenMentah.TabInvMentah[i].Tgl.hari, tabInvenMentah.TabInvMentah[i].Tgl.bulan, tabInvenMentah.TabInvMentah[i].Tgl.tahun);
+			writeln(ext, tabInvenMentah.TabInvMentah[i].Jumlah);
+			writeln(ext, skip);
+		end;
+		close(ext);
+		
+assign (ext,'InvOlahan.txt');
+rewrite(ext);
+	for i:=1 to tabInvenOlahan.Neff do
+		begin
+			writeln(ext, tabInvenOlahan.TabInvOlahan[i].Nama);
+			writeln(ext, tabInvenOlahan.TabInvOlahan[i].TglBuat.hari, tabInvenOlahan.TabInvOlahan[i].TglBuat.bulan, tabInvenOlahan.TabInvOlahan[i].TglBuat.tahun);
+			writeln(ext, tabInvenOlahan.TabInvOlahan[i].Jumlah);
+			writeln(ext, skip);
+		end;
+		close(ext);
+
+assign (ext,'Resep.txt');
+rewrite(ext);
+	for i:=1 to tabResepnya.Neff do
+		begin
+			writeln(ext, tabResepnya.TabResep[i].Nama);
+			writeln(ext, tabResepnya.TabResep[i].Harga);
+			writeln(ext, tabResepnya.TabResep[i].NButuh);
+			for j:=1 to tabResepnya.TabResep[i].Nbutuh do
+				begin
+					writeln(ext, tabResepnya.TabResep[i].Olah[j]);
+				end;
+		end;
+		close(ext);
+		
+assign (ext,'Simulasi.txt');
+rewrite(ext);
+	for i:=1 to tabSimulasi.Neff do
+		begin
+			writeln(ext, tabSimulasi.Tab[i].Nomor);
+			writeln(ext, tabSimulasi.Tab[i].Tgl.hari, tabSimulasi.Tab[i].Tgl.bulan, tabSimulasi.Tab[i].Tgl.tahun);
+			writeln(ext, tabSimulasi.Tab[i].HariHidup);
+			writeln(ext, tabSimulasi.Tab[i].Energi);
+			writeln(ext, tabSimulasi.Tab[i].KapasitasInventory);
+			writeln(ext, tabSimulasi.Tab[i].TotalBeliMentah);
+			writeln(ext, tabSimulasi.Tab[i].TotalOlahBuat);
+			writeln(ext, tabSimulasi.Tab[i].TotalOlahJual);
+			writeln(ext, tabSimulasi.Tab[i].TotalResepJual);
+			writeln(ext, tabSimulasi.Tab[i].TotalPemasukan);
+			writeln(ext, tabSimulasi.Tab[i].TotalPengeluaran);
+			writeln(ext, tabSimulasi.Tab[i].TotalUang);
+			writeln(ext, skip);
+		end;
+		close(ext);
+		
+end;
+end.
