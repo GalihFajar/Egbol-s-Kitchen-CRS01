7219954c64a772b9055075f6582f5735223ac2e8
Akmal Narendra Sakti <akmalns@users.noreply.github.com>
2018-04-13 21:46:35 +0700

    Added comment

--- f8.pas
+++ f8.pas
@@ -16,32 +16,32 @@ implementation
 	dibuat:string;
 	//ALGORITMA LOKAL
 	begin
-		if (energi=0) then
-			writeln('Energi anda habis. Istirahatlah, makan atau tidur terlebih dahulu untuk memulihkan energi')
-		else
+		if (energi=0) then				//Validasi jumlah energi. Energi harus ada untuk dapat melakukan aksi jual resep
+			writeln('Energi anda habis. Istirahatlah, makan atau tidur terlebih dahulu untuk memulihkan energi')		//Pesan kesalahan
+		else							// Energi >0
 			begin
-				write('Masukkan Resep Yang akan dibuat: ');
-				readln(dibuat);
+				write('Masukkan Resep Yang akan dibuat: ');		//Input masukan resep yang akan dibuat
+				readln(dibuat);									//Baca resep
 
-				indeks:=1;
-				cariIndeksResep(a,indeks,dibuat);
-				if (indeks>a.Neff) then
-					writeln('Resep Tidak ditemukan, ulangi perintah dengan resep yang tepat')
+				indeks:=1;										//Assign variabel perulangan
+				cariIndeksResep(a,indeks,dibuat);				//Memanggil prosedur cariresep untuk mencari informasi resep di dalam array resep
+				if (indeks>a.Neff) then							//Validasi, bila indeks melebihi Neff array resep, maka resep tidak ditemukan
+					writeln('Resep Tidak ditemukan, ulangi perintah dengan resep yang tepat')	//Pesan kesalahan
 				else
 				begin
-					if (not searchSemua(a.TabResep[indeks],b,c)) then
-						writeln('Bahan yang diperlukan tidak lengkap, pastikan bahan mentah dan bahan olahan ada di dalam inventori')
-					else
+					if (not searchSemua(a.TabResep[indeks],b,c)) then		//Mencari semua bahan yang dibutuhkan untuk membuat resep, True bila semua bahan ditemukan di dalam inventori
+						writeln('Bahan yang diperlukan tidak lengkap, pastikan bahan mentah dan bahan olahan ada di dalam inventori') //Pesan kesalahan bila bahan mentah/olahan tidak ditemukan
+					else		//Semua bahan telah ditemukan
 					begin
-						kurangiSemua(a.TabResep[indeks],b,c);
-						uang:=uang+a.TabResep[indeks].harga;
-						pemasukan:=pemasukan+a.TabResep[indeks].harga;
-						energi:=energi-1;
+						kurangiSemua(a.TabResep[indeks],b,c);		//Mengurangi kuantitas semua bahan dengan 1
+						uang:=uang+a.TabResep[indeks].harga;		//Menambah uang
+						pemasukan:=pemasukan+a.TabResep[indeks].harga;	//Menambah pemasukan
+						energi:=energi-1;							//Mengurangi energi
 					end;
 				end;
 			end;
-		evaluateInvOlahan(c,t);
-		evaluateInvMentah(b,t,d);
+		evaluateInvOlahan(c,t);			//Evaluasi inventori olahan, bila jumlah bahan olahan=0 (habis) maka bahan dihapus dari array
+		evaluateInvMentah(b,t,d);		//Evaluasi inventori mentah, bila jumlah bahan mentah=0 (habis) maka bahan dihapus dari array
 	end;
 
 end.
