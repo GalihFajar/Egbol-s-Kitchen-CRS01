2f028ce19b7ba524b3f9fb7895351e3fba0d3f9f
novendraw <ekanovendra726@gmail.com>
2018-04-13 16:14:54 +0700

    Commented

--- f15.pas
+++ f15.pas
@@ -11,34 +11,38 @@ implementation
 procedure CariResep(var Input: resepnya);
 
 var
-i,j:integer;
-search:string;
-status:boolean;
+  i,j:integer;
+  search:string;
+  status:boolean;
 
 begin
-write('Masukkan Nama Resep Yang Akan Dicari : ');
-readln(search);
-writeln;
-writeln('Hasil Pencarian Resep :');
-for i:=1 to Input.neff do begin
-	if search=Input.TabResep[i].Nama then begin
-		writeln;
-		status:=true;
-		writeln('Nama Resep : ',Input.TabResep[i].Nama);
-		writeln('Harga Jual : ',Input.TabResep[i].Harga);
-		writeln('Banyak Bahan : ',Input.TabResep[i].NButuh);
-		for j:=1 to Input.TabResep[i].NButuh do begin
-			writeln('Nama Bahan Ke-',j,' : ',Input.TabResep[i].Olah[j]);
-		end;
-	end;
-end;
-if status=false then begin
-	writeln;
-	writeln('Resep Tidak Ditemukan');
-	writeln;
-end else begin	
-	writeln;
-end;
+  write('Masukkan Nama Resep Yang Akan Dicari : ');
+  readln(search);
+  writeln;
+  writeln('Hasil Pencarian Resep :');
+  for i:=1 to Input.neff do
+  begin
+    if search=Input.TabResep[i].Nama then
+    begin
+      writeln;
+      status:=true;
+      writeln('Nama Resep : ',Input.TabResep[i].Nama);
+      writeln('Harga Jual : ',Input.TabResep[i].Harga);
+      writeln('Banyak Bahan : ',Input.TabResep[i].NButuh);
+      for j:=1 to Input.TabResep[i].NButuh do
+      begin
+        writeln('Nama Bahan Ke-',j,' : ',Input.TabResep[i].Olah[j]);
+      end;
+    end;
+  end;
+  if status=false then
+  begin
+    writeln;
+    writeln('Resep Tidak Ditemukan');
+    writeln;
+  end
+  else begin
+    writeln;
+  end;
 end;
 end.
-
