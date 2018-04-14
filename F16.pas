a78c22bb16ac2da697a06dab6c90c57e51c3d1d2
Galih Fajar <galihfajar00@gmail.com>
2018-04-14 20:54:40 +0700

    Update F17

--- F16.pas
+++ F16.pas
@@ -1,8 +1,7 @@
 unit F16;
 //Unit F16-tambahResep
 //Dibuat Oleh : Galih Fajar Fitra Ady/16517258
-//Versi : 08/04/2018
-//Try Now
+
 
 {Deskripsi Program}
 {Program berfungsi untuk menambah resep ke array resep yang telah ada}
@@ -18,9 +17,9 @@ unit F16;
 
 interface
   uses tipe;
-  procedure tambahResep(x : InvMentah; y : InvOlahan ;var masukan : tempResep); //Prosedur utama menambah resep.
-  procedure TambahBahan(max: integer;batas : integer;x : InvMentah; y : InvOlahan; var masukan : tempResep); //Prosedur menambah bahan mentah.
-  function CekUjung(masukan : tempResep):integer; //Prosedur cek jumlah resep yang tersedia.
+  procedure tambahResep(x : InvMentah; y : InvOlahan ;var masukan : resepnya); //Prosedur utama menambah resep.
+  procedure TambahBahan(max: integer;batas : integer;x : InvMentah; y : InvOlahan; var masukan : resepnya); //Prosedur menambah bahan mentah.
+  function CekUjung(masukan : resepnya):integer; //Prosedur cek jumlah resep yang tersedia.
 implementation
   uses searching;
 
@@ -40,51 +39,51 @@ implementation
       searchSemuaV1 := temp1 or temp2;
   	end;
 
-  procedure TambahBahan(max: integer;batas : integer;x : InvMentah; y : InvOlahan; var masukan : tempResep);
+  procedure TambahBahan(max: integer;batas : integer;x : InvMentah; y : InvOlahan; var masukan : resepnya);
     var
       i : integer;
     begin
       for i := 1 to batas do
         begin
-          write('Bahan ',i,' : ');readln(masukan[max].Olah[i]); //Memasukan data bahan mentah ke array bahan mentah.
-          if(searchSemuaV1(masukan[max].Olah[i],x,y) = False) then
+          write('Bahan ',i,' : ');readln(masukan.TabResep[max].Olah[i]); //Memasukan data bahan mentah ke array bahan mentah.
+          if(searchSemuaV1(masukan.TabResep[max].Olah[i],x,y) = False) then
             begin
               writeln	('Bahan tidak ada, ulangi masukan.');
               repeat
-                write('Bahan ',i,' : ');readln(masukan[max].Olah[i]);
-                if (searchSemuaV1(masukan[max].Olah[i],x,y) = False) then
+                write('Bahan ',i,' : ');readln(masukan.TabResep[max].Olah[i]);
+                if (searchSemuaV1(masukan.TabResep[max].Olah[i],x,y) = False) then
                   writeln('Bahan tidak ada, ulangi masukan.')
-              until (searchSemuaV1(masukan[max].Olah[i],x,y) = True);
+              until (searchSemuaV1(masukan.TabResep[max].Olah[i],x,y) = True);
             end;
         end;
     end;
 
-  function CekUjung(masukan : tempResep):integer;
+  function CekUjung(masukan : resepnya):integer;
     var
       i : integer;
     begin
       CekUjung := 0;
       i := 1;
       repeat
-        if(masukan[i].Nama <> '') then
+        if(masukan.TabResep[i].Nama <> '') then
           CekUjung := CekUjung + 1
         else
           CekUjung := CekUjung;
         i := i + 1;
-      until(masukan[i].Nama = '') //Menambah NEff sampai array kosong.
+      until(masukan.TabResep[i].Nama = '') //Menambah NEff sampai array kosong.
     end;
 
-  procedure tambahResep(x : InvMentah; y : InvOlahan ;var masukan : tempResep);
+  procedure tambahResep(x : InvMentah; y : InvOlahan ;var masukan : resepnya);
     var
       max : integer;
     begin
       max := (CekUjung(masukan)) + 1;
-      write('Nama Resep : ');readln(masukan[max].Nama); //Menerima input pengguna berupa nama resep.
-      write('Harga : ');readln(masukan[max].Harga); // Menerima input pengguna berupa harga resep (Belum dapat menaksir harga dari konstanta harga bahan mentah).
+      write('Nama Resep : ');readln(masukan.TabResep[max].Nama); //Menerima input pengguna berupa nama resep.
+      write('Harga : ');readln(masukan.TabResep[max].Harga); // Menerima input pengguna berupa harga resep (Belum dapat menaksir harga dari konstanta harga bahan mentah).
       repeat
-        write('Jumlah : ');readln(masukan[max].NButuh); //Menerima jumlah bahan yang dibutuhkan sampai memenuhi syarat.
-      until ((masukan[max].NButuh >= 2) and (masukan[max].NButuh <= 20));
+        write('Jumlah : ');readln(masukan.TabResep[max].NButuh); //Menerima jumlah bahan yang dibutuhkan sampai memenuhi syarat.
+      until ((masukan.TabResep[max].NButuh >= 2) and (masukan.TabResep[max].NButuh <= 20));
 
-      TambahBahan(max,masukan[max].NButuh,x,y,masukan); //Menerima input pengguna berupa nama bahan yang diperlukan (Belum dapat memvalidasi apakah bahan mentah ada dallam list atau tidak).
+      TambahBahan(max,masukan.TabResep[max].NButuh,x,y,masukan); //Menerima input pengguna berupa nama bahan yang diperlukan (Belum dapat memvalidasi apakah bahan mentah ada dallam list atau tidak).
     end;
 end.
