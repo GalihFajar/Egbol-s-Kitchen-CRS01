79b3b6f9b508d4d931f105aa330f94e6eda413c1
edgyballer <38172799+edgyballer@users.noreply.github.com>
2018-04-14 22:08:17 +0700

    Add files via upload

--- f3f4.pas
+++ f3f4.pas
@@ -1,17 +1,23 @@
-unit f3Simulasi;
-
+unit f3f4;
 
 interface
+
+uses tipe,F1load,f2exit,f5,f6,f7,f8,f9,f10,f11tidur,f12statistik,f13,f14,f15,f16,f17;
+
 procedure simulasi(var tabBahanMentah : Mentah; var tabBahanOlahan : Olahan; var tabInvenMentah : InvMentah; var tabInvenOlahan : InvOlahan;
-				var tabResepnya : Resepnya; var tabSimulasi : tSimulasi; reseptemp : tempresep);
+				var tabResepnya : Resepnya; var tabSimulasi : tSimulasi; duid : LongInt; power:Integer; countmakan:Integer; countist:Integer;
+					harihidup:Integer; simul:Simulasi);
 
 implementation
 
 procedure simulasi(var tabBahanMentah : Mentah; var tabBahanOlahan : Olahan; var tabInvenMentah : InvMentah; var tabInvenOlahan : InvOlahan;
-				var tabResepnya : Resepnya; var tabSimulasi : tSimulasi; reseptemp:tempresep);
+				var tabResepnya : Resepnya; var tabSimulasi : tSimulasi; duid : LongInt; power:Integer; countmakan:Integer; countist:Integer;
+					harihidup:Integer; simul:Simulasi);
 var
 	n	: string;
 	date: Tanggal;
+	emax : integer;
+	energi : integer;
 begin
 write('>>');
 readln(n);
@@ -19,25 +25,30 @@ while not(n='Stop') do
 	begin
 		begin
 			case n of
-			{'Beli bahan'		: 	begin
-										belibahan(tSimulasi.Tgl, tabInvenMentah,tabBahanMentah);
-									end;}
-			{'Olah bahan'		: 	begin
-										olahBahan(tabInvenMentah,tabInvenOlahan,tabBahanOlahan,tabInvenOlahan.TglBuat, moni, energi);
-									end;}
-			{'Jual olahan'		:	begin
-										jualOlahan(tabInvenOlahan,}
-			//8 belum
-			{('Makan'			:	begin
-										makan(energi,countmakan,emax);
-									end;}
-			{'Istirahat'		:	begin
-										istirahat(energi,countist,emax);
-									end;}
-			//11 belum
-			'Lihat statistik'	: 	begin
-										Lihatstatistik(tabSimulasi);
+			'Beli bahan'		: 	begin
+										belibahan(date,duid,power, tabInvenMentah,tabBahanMentah,tabSimulasi.Tab[simul.nomor].TotalPengeluaran);
 									end;
+			'Olah bahan'		: 	begin
+										olahBahan(tabInvenMentah,tabInvenOlahan, tabBahanOlahan,date, power);
+									end;
+			'Jual olahan'		:	begin
+										jualOlahan(tabInvenOlahan, date, tabBahanOlahan,tabSimulasi.tab[simul.nomor].TotalPemasukan, duid, power);
+									end;
+			'Jual resep'		:	begin
+										jualResep(date, tabResepnya, tabInvenMentah, tabInvenOlahan, tabBahanMentah, duid, tabSimulasi.tab[simul.nomor].TotalPemasukan, power);
+									end;
+			'Makan'				:	begin
+										makan(power,countmakan,emax);
+									end;
+			'Istirahat'			:	begin
+										istirahat(energi,countist,emax);
+									end;
+			'Tidur'				:	begin
+										Tidur(date, tabInvenOlahan, tabInvenMentah, tabBahanMentah, power, harihidup);
+									end;
+			//'Lihat statistik'	: 	begin
+			//							Lihatstatistik(simul, date, harihidup, power, capacity);
+			//						end;
 			'Tampilkan inventory':	begin
 										TampilkanInventory(tabInvenMentah,tabInvenOlahan);
 									end;
@@ -48,13 +59,18 @@ while not(n='Stop') do
 										CariResep(tabResepnya);
 									end;
 			'Tambah resep'		:	begin
-										TambahResep(tabInvenMentah,tabInvenOlahan,tempresep);
+										TambahResep(tabInvenMentah,tabInvenOlahan,tabResepnya);
 									end;
-			//f17 belom
+			'Upgrade Inventory'	:	begin
+										upgradeInventori(simul);
+									end;
 		end;
+		
+	end;
 		write('>>');
 		readln(n);
-	end;
-		Lihatstatistik(tabSimulasi);
+		{Lihatstatistik(tabSimulasi);}
 
+end;
+end;
 end.
