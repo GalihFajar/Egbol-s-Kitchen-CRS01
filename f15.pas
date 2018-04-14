f04ec9bcc4d148e5f653577db5c313f142a98fb1
novendraw <ekanovendra726@gmail.com>
2018-04-14 21:10:37 +0700

    F15

--- f15.pas
+++ f15.pas
@@ -27,7 +27,8 @@ begin
   readln(search); 									// Membaca nama resep yang dicari dari input pengguna
   writeln; 											// Menuliskan newline di layar
   writeln('Hasil Pencarian Resep :'); 				// Menuliskan 'Hasil Pencarian Resep :' di layar
-  status:=false;
+  status:=false;                // mengassign nilai False ke variabel status
+
   // Melakukan pengulangan sebanyak jumlah resep
   for i:=1 to Input.neff do
   begin
