948674f51bd382af9489330203d9a5d90ff2e593
Galih Fajar <galihfajar00@gmail.com>
2018-04-22 19:55:01 +0700

    Update

--- F17.pas
+++ F17.pas
@@ -3,20 +3,23 @@ unit F17;
 interface
 	uses tipe;
 	procedure upgradeInventori(var inventori : integer; var uang : longint; var energi : integer);
-	
+	{I.S. Tersedia variabel inventori, uang dan energi}
+	{F.S. Jika berhasil, inventori bertambah, uang berkurang, dan energi berkurang}
 implementation
 	procedure upgradeInventori(var inventori : integer; var uang : longint; var energi : integer);
-		begin
-			if (uang >= 50000 ) then
-				begin
-				writeln('Upgrade berhasil!');
+	{I.S. Tersedia variabel inventori, uang dan energi}
+	{F.S. Jika berhasil, inventori bertambah, uang berkurang, dan energi berkurang}
+	begin
+		if (uang >= 50000 ) then //Validasi uang, jika mencukupi akan diupgrade.
+			begin
+				writeln('Upgrade berhasil!'); 
 				uang := uang - 50000;
 				inventori := inventori + 25;
 				energi := energi - 1;
-				end
+			end
 			else
 				writeln('Uang tidak mencukupi.')
-		end;
+	end;
 
 end.
 			
